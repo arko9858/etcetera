9ae4fae69641e8cbf609f03096a76d77e458b88d
Arko Sakib <arko9858@gmail.com>
2020-04-14 12:58:30 +0600

    more test stuff

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -1,10 +1,14 @@
 export default (req, res) => {
   const {method} = req
 
-  //   if (method === "GET") {
-  //     res.status(200).send("It's working")
-  //   } else {
-  const message = method ? method : "Error" + " request"
-  res.json({msg: message})
-  //   }
+  const envVars = process.env ? process.env : "vars not found"
+  const secret = process.env.jwtSecret
+    ? process.env.jwtSecret
+    : "secret not found"
+
+  if (method === "GET") {
+    res.status(200).send("It's working")
+  } else {
+    res.json({allEnvVars: envVars, secret})
+  }
 }
