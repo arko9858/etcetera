5eed0a1bcf50a59055a53b6024d9bb81c7340f24
Arko Sakib <arko9858@gmail.com>
2020-04-15 12:58:37 +0600

    another test

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -1,20 +1,45 @@
-import mongoDb from '../../../backend/middlewares/mongoDB'
-import nextConnect from 'next-connect'
+import {MongoClient} from "mongodb"
 
-const handler = nextConnect()
+export default async (req, res) => {
+  try {
+    switch (req.method) {
+      case "GET":
+        const mongoURI =
+          "mongodb+srv://arko9858:Do3VytqKneGup4Kp@cluster0-b2jmx.mongodb.net/etc-db1?retryWrites=true&w=majority"
+        const client = new MongoClient(mongoURI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
+        if (!client.isConnected()) await client.connect()
+        const db = client.db("etc-db1")
+        let doc = await db.collection("users").findOne()
+        return res.status(200).json(doc)
 
-handler.use(mongoDb)
+      default:
+        return res.status(405).json({msg: "invalid request"})
+    }
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send("Server error")
+  }
+}
 
+// import mongoDb from '../../../backend/middlewares/mongoDB'
+// import nextConnect from 'next-connect'
 
-handler.get(async (req, res) => {
+// const handler = nextConnect()
 
-  let doc = await req.db.collection('users').findOne()
-  console.log(doc);
-  res.json(doc);
-});
+// handler.use(mongoDb)
 
-handler.post(async (req,res)=>{
-  res.json({msg:"this is a post req"})
-})
+// handler.get(async (req, res) => {
 
-export default handler;
+//   let doc = await req.db.collection('users').findOne()
+//   console.log(doc);
+//   res.status(200).json(doc);
+// });
+
+// handler.post(async (req,res)=>{
+//   res.json({msg:"this is a post req"})
+// })
+
+// export default handler;
